7198b108f74a2177d5606912177bb7caa46828e2
ViniciusChagas95 <viniciuschagas95@hotmail.com>
2023-08-31 08:57:02 -0300

    Iniciando routers. createBrowserRouterProvider,Objeto de ROtas

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//BLOCO DE ROTAS
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import EditarProdutos from './pages/EditarProdutos'
+import Produtos from './routes/Produtos.jsx'
+import Erro404 from './routes/Erro404.jsx'
+import Home from './routes/Home.jsx'  
+
+const router = createBrowserRouter({
+  routes: [
+    { path: '/', element: <App/>, 
+      children: [
+        {path: '/', element: <Home/>},
+        {path: '/produtos', element: <Produtos/>},
+        {path: '/editar/produtos/:id', element: <EditarProdutos/>},
+        {path: '*', element: <Erro404/>}
+      ] 
+    },
+  
+  ],
+})
+//BLOCO DE ROTAS
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
