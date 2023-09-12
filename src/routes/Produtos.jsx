4ae01cb7860a433019dd06f4ec0aa1af96c6df92
ViniciusChagas95 <viniciuschagas95@hotmail.com>
2023-09-12 09:13:55 -0300

    estilizando a tabela com css.module

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,45 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
-import classeEstilos from "./Produtos.modules.css"
-export default function Produtos() {
+import classes from "./Produtos.module.css";
 
+export default function Produtos() {
   return (
     <div>
-        <h1>Produtos de INFORMÁTICA - FIPAPI</h1>
-        <p>Os Melhores Produtos do Mercado</p>
-        
-        <table className={classeEstilos.estiloTabela}>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>NOME</th>
-                    <th>DESCRIÇÃO</th>
-                    <th>PREÇO</th>
-                </tr>
-            </thead>
+      <h1>Produtos de INFORMÁTICA - FIPAPI</h1>
+      <p>Os Melhores Produtos do Mercado</p>
 
-            <tbody>
-                {ListaProdutos.map((produto,indice)=>(
+      <table className={classes.estilo}>
+        <thead>
+          <tr>
+            <th className={classes.tableHeaders}>ID</th>
+            <th className={classes.tableHeaders}>NOME</th>
+            <th className={classes.tableHeaders}>DESCRIÇÃO</th>
+            <th className={classes.tableHeaders}>PREÇO</th>
+            <th className={classes.tableHeaders}>EDITAR</th>
+          </tr>
+        </thead>
 
-                      <tr key={indice}>
-                        <td>{produto.id}</td>
-                        <td>{produto.nome}</td>
-                        <td>{produto.desc}</td>
-                        <td>{produto.preco}</td>
-                      </tr>
+        <tbody>
+          {ListaProdutos.map((produto, indice) => (
+            <tr key={indice}>
+              <td className={classes.tableData}>{produto.id}</td>
+              <td className={classes.tableData}>{produto.nome}</td>
+              <td className={classes.tableData}>{produto.desc}</td>
+              <td className={classes.tableData}>{produto.preco}</td>
+              <td className={classes.tableData}>
+                <Link to={`/editar/produtos/${produto.id}`}>Editar</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
 
-                ))}
-            </tbody>
-
-            <tfoot>
-                <tr>
-                    <td colSpan={3}>PRODUTOS</td>
-                </tr>
-            </tfoot>
-        </table>
-        
-
-
+        <tfoot>
+          <tr>
+            <td colSpan={3}>PRODUTOS</td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
-  )
+  );
 }
